4dd44fd1205c5b993aa2023721501a1cf123f19c
Devin Nguyen <diagonr@gmail.com>
2018-04-30 17:02:53 -0500

    SMS Broadcast Receiver Implementation.
    A receiver is required to read a message given a context and intent.

---
+++ app/src/main/java/com/sms/activities/SMSBroadcastReceiver.java
@@ -0,0 +1,38 @@
+package com.sms.activities;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+public class SMSBroadcastReceiver extends android.content.BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        //Specify bundle based on SMS protocol
+        assert bundle != null;
+        Object[] objects = (Object[]) bundle.get("pdus");
+        SmsMessage sms[] = new SmsMessage[0];
+        if (objects != null) {
+            sms = new SmsMessage[objects.length];
+        }
+        Intent newIntent = new Intent(context, MessageActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        String message;
+        String originNm = "";
+        StringBuffer sb = new StringBuffer();
+        assert objects != null;
+        for(int i = 0; i < objects.length; i++){
+            sms[i] = SmsMessage.createFromPdu((byte[]) objects[i]);
+            message = sms[i].getDisplayMessageBody();
+            originNm = sms[i].getDisplayOriginatingAddress();
+            sb.append(message);
+            abortBroadcast();
+        }
+        newIntent.putExtra("originNm", originNm);
+        newIntent.putExtra("message", new String(sb));
+        //Start the messageDisplay
+        context.startActivity(newIntent);
+
+    }
+}
